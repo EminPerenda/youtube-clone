be234747fe16580166787a2e288bef25db58baf8
Emin <eminperenda@gmail.com>
2026-06-29 12:30:08 +0200

    fourth commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import "./Home.css";
-export const Home = () => {
-  return <div>Home</div>;
+import { Sidebar } from "../../Components/Sidebar/Sidebar";
+import { Feed } from "../../Components/Feed/Feed";
+
+export const Home = ({ sidebar }) => {
+  return (
+    <>
+      <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <div className="feed">
+          <Feed />
+        </div>
+      </div>
+    </>
+  );
 };
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./Video.css";
+import { PlayVideo } from "../../Components/PlayVideo/PlayVideo";
+import { Recommended } from "../../Components/Recommended/Recommended";
 export const Video = () => {
-  return <div>Video</div>;
+  return (
+    <div className="play-container">
+      <PlayVideo />
+      <Recommended />
+    </div>
+  );
 };
